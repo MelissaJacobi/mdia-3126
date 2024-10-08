ee0fc569f754ae301307ff629fc3d0da198d1cb0
MelissaJacobi <144720727+MelissaJacobi@users.noreply.github.com>
2024-10-08 16:13:49 -0700

    api

---
+++ webapp-lab06/.history/src/app/api/page_20241008144110.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const DATA_URL = "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json";
+
+
+export default function Page() {
+    return(
+        <div className="p-4 bg-yellow-300">
+            <header>
+                <h1>Welcom to my product page</h1>
+                <button className="border-neutral-200 bg-black px-6" onClick={() => void}>Fetch Products!</button>
+            </header>
+            Hello welcome to my API page T_T
+        </div>
+    );
+}
+
---
+++ webapp-lab06/.history/src/app/api/page_20241008144522.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import {useState} from 'react';
+"use client"
+
+const DATA_URL = "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json";
+const [data, setData] = useState(null);
+
+export default function Page() {
+    return(
+        <div className="p-4 bg-blue-300">
+            <header className="border-4 border-black p-4 mb-4">
+                <h1>Welcom to my product page</h1>
+                <button className="border-neutral-200 bg-black px-6" 
+                //onClick={}
+                >Fetch Products!</button>
+            </header>
+            <div>
+                <ul className="border-4 border-black p-4 mb-4">
+                    <li>blah</li>
+                    <li>blah</li>
+                </ul>
+            </div>
+            Hello welcome to my API page T_T
+        </div>
+    );
+}
+
